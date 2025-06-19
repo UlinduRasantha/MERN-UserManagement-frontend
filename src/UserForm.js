1e0f122af76ec128bd183218def9b1b94506ce1f
URSamarappuli <22ug1-0465@sltc.ac.lk>
2025-06-19 23:30:53 +0530

    update

--- src/UserForm.js
+++ src/UserForm.js
@@ -29,11 +29,11 @@ const UserForm = ({ addUser, updateUser, submitted, data, isEdit }) => {
                 display: 'block',
             }}
         >
-            <Grid gridColumn="span 12" >
+            <Grid size={{ xs: 12 }} >
                 <Typography component= {'h1'} sx={{color: '#000000'}}>User Form</Typography>
             </Grid>
 
-            <Grid gridColumn="span 12" md={6} sx={{display: 'flex'}}>
+            <Grid size={{ xs: 12, sm: 6 }} sx={{display: 'flex'}}>
                 <Typography 
                     component={'label'} 
                     htmlFor="id"
@@ -57,7 +57,7 @@ const UserForm = ({ addUser, updateUser, submitted, data, isEdit }) => {
                 />
             </Grid>
 
-            <Grid item xs={12} sm={6} sx={{display: 'flex'}}>
+            <Grid size={{ xs: 12, sm: 6 }} sx={{display: 'flex'}}>
                 <Typography 
                     component={'label'} 
                     htmlFor="id"
